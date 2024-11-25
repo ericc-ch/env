0d789e3e0c88ca784c9e5f12bd82d2ee52874218
Erick Christian <erickchristian48@gmail.com>
2024-11-25 22:03:33 +0700

    feat: npm release

---
+++ scripts/build_npm.ts
@@ -0,0 +1,53 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "@deno/dnt";
+
+const OUTPUT_DIR = "./.npm";
+const OUTPUT_LICENSE = OUTPUT_DIR + "/LICENSE";
+const OUTPUT_README = OUTPUT_DIR + "/README.md";
+const PACKAGE_NAME = "@echristian/env";
+const VERSION = "0.0.2";
+
+await emptyDir(OUTPUT_DIR);
+
+await build({
+  packageManager: "pnpm",
+  entryPoints: [
+    {
+      name: "./bun",
+      path: "./src/bun.ts",
+    },
+    {
+      name: "./node",
+      path: "./src/node.ts",
+    },
+    {
+      name: "./deno",
+      path: "./src/deno.ts",
+    },
+  ],
+  outDir: OUTPUT_DIR,
+  shims: {},
+  typeCheck: false,
+  scriptModule: false,
+  test: false,
+  package: {
+    // package.json properties
+    name: PACKAGE_NAME,
+    version: VERSION,
+    description:
+      "Small, cross runtime utility function to get environment variables.",
+    license: "MPL-2.0",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/ericc-ch/env.git",
+    },
+    bugs: {
+      url: "https://github.com/ericc-ch/env/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", OUTPUT_LICENSE);
+    Deno.copyFileSync("README.md", OUTPUT_README);
+  },
+});
